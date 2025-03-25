5ef1c87beed0c277bcfbb6804a523902aa57c24a
mr. mateusz <114559755+mrmateusz123@users.noreply.github.com>
2025-03-26 00:33:24 +0100

    Add files via upload

--- TETRAS/script.js
+++ TETRAS/script.js
@@ -3,13 +3,15 @@ audio.loop = true;
 const volumeControl = document.getElementById("volumeControl");
 const optionsDiv = document.querySelector('.options');
 const menuDiv = document.querySelector('.start');
+const infoDiv = document.querySelector('.info-text');
 const highScoreDisplay = document.querySelector('.high-score');
 
 
 function soulsold() {
     audio.play();
     document.querySelector('.disclaimer').style.display = "none";
-    document.querySelector('.infotext').style.display = "block";
+    document.querySelector('.main').style.display = "block";
+    document.querySelector('.info-text').style.display = "block";
     document.querySelector('.start').style.display = "flex";
 }
 
@@ -27,10 +29,12 @@ function startGame(){
 function options() {
     optionsDiv.style.display = "flex";
     menuDiv.style.display = "none";
+    infoDiv.style.display = "none";
 }
 function backtomenu(){
     optionsDiv.style.display = "none";
     menuDiv.style.display = "flex";
+    infoDiv.style.display = "block";
 }
 volumeControl.addEventListener("input", function() {
     audio.volume = this.value;
